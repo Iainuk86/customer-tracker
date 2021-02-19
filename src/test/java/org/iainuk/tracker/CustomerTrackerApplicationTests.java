7dc30c5ea1dac7ff6d024ca6c4f47c9763f65767
Iainuk86 <iainuk1986@gmail.com>
2021-02-19 18:03:15 +1100

    Added Maven

---
+++ src/test/java/org/iainuk/tracker/CustomerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iainuk.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
